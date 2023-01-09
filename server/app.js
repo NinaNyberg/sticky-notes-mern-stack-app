cff1b4d452c99097a1a0377753273b313183fcd6
Nina N <nina.b.nyberg@gmail.com>
2023-01-10 00:35:22 +0200

    Test

--- server/app.js
+++ server/app.js
@@ -29,6 +29,14 @@ app.use(
     credentials: false
   })
 );
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Expose-Headers', '*');
+  next();
+});
+
 app.use(express.json());
 app.use(
   expressSession({
